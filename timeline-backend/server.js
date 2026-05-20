198c0fec97920b7bbaae087771c5b09720039feb
Shamima <shamima2802@gmail.com>
2026-05-20 14:13:16 +0530

    env file

--- timeline-backend/server.js
+++ timeline-backend/server.js
@@ -9,9 +9,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: '*', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-device-mac'] 
-}))
+}));
+
+app.options(/.*/, cors()); 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
